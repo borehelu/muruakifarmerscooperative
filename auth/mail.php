bbb1a6c5059c09a58f78b0994a4c9c75bb2c5c01
Helu Bore <82686085+borehelu@users.noreply.github.com>
2022-10-26 18:43:54 +0300

    Update mail.php

--- auth/mail.php
+++ auth/mail.php
@@ -11,8 +11,8 @@ try {
 	$mail->isSMTP();											
 	$mail->Host	 = 'smtp.gmail.com;';					
 	$mail->SMTPAuth = true;							
-	$mail->Username = 'helussandbox@gmail.com';				
-	$mail->Password = 'rehoboth@2019';						
+	$mail->Username = '';				
+	$mail->Password = '';						
 	$mail->SMTPSecure = 'tls';							
 	$mail->Port	 = 587;
 
